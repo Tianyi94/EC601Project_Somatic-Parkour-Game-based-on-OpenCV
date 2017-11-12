d5ef77390b8f06914ba2af236a1736a25d7344ed
Chao Ma <mc163@bu.edu>
2017-11-12 15:04:19 -0500

    Update detectFace.py

--- FaceDetction/detectFace.py
+++ FaceDetction/detectFace.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python
 import numpy as np
 import cv2
-from time import sleep
+#from time import sleep
 from medianFilter import median_2d_time_filter
-
 median = median_2d_time_filter()
-median0 = median_2d_time_filter()
-median1 = median_2d_time_filter()
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 # capture video
 cap = cv2.VideoCapture(0)
-x=0
-y=0
-w=0
-h=0
-facesmedian=[()]
-faceslist=[[]]
-font = cv2.FONT_HERSHEY_SIMPLEX
+# data from face_cascade
+x = 0
+y = 0
+w = 0
+h = 0
+# center of the face
+x_mid = 0
+y_mid = 0
+# left / right / up flag
+x_axis = 0
+y_axis = 0
+# store data after midian filter
+facesmedian = [()]
+faceslist = [[]]
 
 while True:
-	left=0
-	right=0
 #	sleep(0.02) # Time in seconds.
+
 # read captured images
 	ret,img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # detect face
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-# if success to get face
+# if success to get face, push it into median
     	if type(faces) != tuple:
 		median.push(faces[0][0], faces[0][1], faces[0][2], faces[0][3])
 	if type(faces) == tuple:
@@ -36,7 +40,7 @@ while True:
 
 #	print 'faces=',faces
 
-# store data after median filter
+# store data after median filter into faceslist
 	facesmedian[0] = median.median()
 	faceslist[0] = list(facesmedian[0])
 	for i in range(4):
@@ -52,11 +56,51 @@ while True:
     		roi_gray = gray[y:y+h, x:x+w]
     		roi_color = img[y:y+h, x:x+w]
 
+# draw blue left line
+		cv2.line(img,(250,0),(250,480),(255,0,0),2)
+# draw blue right line
+		cv2.line(img,(390,0),(390,480),(255,0,0),2)
+# draw green up line
+		cv2.line(img,(0,200),(640,200),(0,255,0),2)
+
+# find the center of the face
+		x_mid = x + w/2
+		y_mid = y + h/2
+
+# the img is 640 x 480
+# find the position of center to determain left / right / up
+# note : the img showed is mirrored from actual movement
+# x axis
+		# left side
+		if (x_mid > 390):
+			x_axis = 1
+			print 'left ', x_axis
+		# right side
+		elif (x_mid < 250):
+			x_axis = 2
+			print 'right ', x_axis
+		else:
+			x_axis = 0
+			print 'middle ', x_axis
+# y axis		
+		# upper side
+		if (y_mid < 200):
+			y_axis = 3
+			print 'up ', y_axis
+		# lower side
+		else:
+			y_axis = 0
+			print 'down ', y_axis
+
+		print 'x = ', x
+		print 'y = ', y
+		print 'w = ', w
+		print 'h = ', h
 # show image
-	cv2.imshow('img',img)	
+	cv2.imshow('img', img)	
 
 # press q to quit
-	if (cv2.waitKey(1)==ord('q')):
+	if (cv2.waitKey(1) == ord('q')):
 		break
 # free and close
 cap.release()

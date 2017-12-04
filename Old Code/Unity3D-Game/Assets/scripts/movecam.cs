3e6b80366a269dd8141a6409e5c03785e91d6e3e
Tianyi Zhang <31803844+Tianyi94@users.noreply.github.com>
2017-12-03 20:38:00 -0500

    Changed the structures

---
+++ Old Code/Unity3D-Game/Assets/scripts/movecam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movecam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, GM.vertVel, 4 * GM.zVelAdj);
+
+    }
+}
---
+++ Old Code/Unity3D-Game/Assets/scripts/stats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stats : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.name == "cointxt")
+        {
+            GetComponent<TextMesh>().text = "Coins : " + GM.coinTotal;
+        }
+
+        if (gameObject.name == "timetxt")
+        {
+            GetComponent<TextMesh>().text = "Time : " + GM.timeTotal;
+        }
+
+
+    }
+}

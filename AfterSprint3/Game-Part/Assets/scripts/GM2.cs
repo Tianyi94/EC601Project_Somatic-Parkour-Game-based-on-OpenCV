8c4530c292f6d32829e8a3842f74a403c486df4e
Tianyi Zhang <31803844+Tianyi94@users.noreply.github.com>
2017-12-03 19:20:32 -0500

    add new files

---
+++ AfterSprint3/Game-Part/Assets/scripts/GM2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM2 : MonoBehaviour {
+
+    
+    public static float coinTotal = 0;
+    public static float timeTotal = 0;
+    public static float zVelAdj = 1;
+    public static float pumpkinVel = -3;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeTotal += Time.deltaTime;
+
+    }
+}
---
+++ AfterSprint3/Game-Part/Assets/scripts/run.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class run : MonoBehaviour {
+
+	Rigidbody thisrigid;
+
+	// Use this for initialization
+	void Start () {
+		thisrigid = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		thisrigid.velocity = new Vector3 (Input.acceleration.x, thisrigid.velocity.y, 15.0f);
+
+	}
+}
